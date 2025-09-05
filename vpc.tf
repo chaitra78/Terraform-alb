e1385b95ffa8edbf00cb553e26eaa11a9150e06f
Chaitra N R <chaitra.nr@hcltech.com>
2025-09-05 14:47:56 +0530

    updated1

--- vpc.tf
+++ vpc.tf
@@ -39,7 +39,7 @@ resource "aws_subnet" "public2" {
 # Public Subnet
 resource "aws_subnet" "public3" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1c"
   tags = {
